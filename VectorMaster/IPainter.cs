5e21b70de0f1a2d1bd7ee73e9559e5bd27118a14
CosmoStas <stasbkj@gmail.com>
2020-12-08 17:32:57 +0300

    Some ideaas for IPainter

---
+++ VectorMaster/IPainter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorMaster
+{
+    public interface IPainter
+    {
+        void Paint(Bitmap Bm, Graphics graphics);   
+    }
+}
