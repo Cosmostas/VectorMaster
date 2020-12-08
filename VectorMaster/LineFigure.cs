d66e3c86cd4db8919857fb949e6b272dabc90ed0
CosmoStas <stasbkj@gmail.com>
2020-12-08 17:14:15 +0300

    LineFigure done

---
+++ VectorMaster/LineFigure.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorMaster
+{
+    public class LineFigure : IFigure
+    {
+        private Point _beginLine;
+        private Point _endLine;
+        public LineFigure()
+        {
+
+        }
+        public LineFigure(Point beginLine, Point endLine)
+        {
+            _beginLine = beginLine;
+            _endLine = endLine;
+        }
+
+        public void SetPoint(Point beginLine, Point endLine)
+        {
+            _beginLine = beginLine;
+            _endLine = endLine;
+        }
+        public List<Point> GetPoints()
+        {
+            return new List<Point>() { _beginLine, _endLine};
+        }
+
+    }
+}
