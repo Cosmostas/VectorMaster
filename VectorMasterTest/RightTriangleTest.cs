60caedc06de6f1a3ae5b33a896fed9d7fa7c2e1a
Alexey <alexxd91@gmail.com>
2020-12-20 17:29:56 +0300

    Right Triangle test

---
+++ VectorMasterTest/RightTriangleTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Drawing;
+using VectorPaintNUnitTest;
+
+namespace VectorPaintNUnitTest
+{
+    public class RightTriangleTest
+    {
+        [Test, TestCaseSource(typeof(GetPointMock))]
+        public void SetPoints(Point firstPoint, Point lastPoint, Point[] Expected)
+        {
+            RightTriangleTest rectangle = new RightTriangleTest();
+            rectangle.SetPoint(firstPoint, lastPoint);
+            Point[] actual = rectangle.GetPoints().ToArray();
+            Assert.AreEqual(Expected, actual);
+        }
+    }
+
+    public class GetPointMock : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[] { new Point(0, 0), new Point(10, 10), new Point[]
+                { new Point(0, 0), new Point(0, 10) } };
+            yield return new object[] { new Point(10, 10), new Point(0, 0), new Point[]
+                { new Point(-10, 0), new Point(0, 0) } };
+            yield return new object[] { new Point(0, 10), new Point(10, 0), new Point[]
+                { new Point(0, 0), new Point(10, 10) } };
+            yield return new object[] { new Point(10, 0), new Point(0, 10), new Point[]
+                { new Point(10, 0), new Point(10, 0) } };
+
+        }
+    }
+}
